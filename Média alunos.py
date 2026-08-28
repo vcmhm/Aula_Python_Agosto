2acbf7a50ab0fe9789b1d94b53229aeedee5c737
vcmhm <103142577+vcmhm@users.noreply.github.com>
2026-08-27 21:18:33 -0300

    Add files via upload

---
+++ Média alunos.py
@@ -0,0 +1,30 @@
+
+
+
+nome   = input ("Nome do aluno:")
+nota1  = float (input("Qual a sua primeira nota?"))
+nota2  = float (input("Qual a sua segunda nota?"))
+nota3  = float (input("Qual a sua terceira nota?"))
+nota4  = float (input("Qual a sua quarta nota?"))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+if media >= 7:
+    print("APROVADO e sua média é:", media)
+elif media == 5:
+    print("RECUPERAÇÃO e sua média é:", media)    
+else: 
+    print("REPROVADO e sua média é:", media)     
+
+
+
+
+
+
+
+
+
+
+
+ 
+
